683851fab894e94fbe13759243adb561f1ccb8d7
Rosalba <rous092006@gmail.com>
2024-10-27 21:48:41 -0500

    añadí un nuevo diseño

---
+++ ecstasy/ecstasy/src/test/java/com/ecstasy/s/EcstasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecstasy.s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcstasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
