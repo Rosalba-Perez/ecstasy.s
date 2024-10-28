23b491dc13784ec0107f1f9d7335a74fee9e5190
Rosalba <rous092006@gmail.com>
2024-10-28 12:35:44 -0500

    modifique el controlador

---
+++ ecstasy/ecstasy/src/main/java/com/ecstasy/s/controlador.java
@@ -0,0 +1,38 @@
+package com.ecstasy.s;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
+public class controlador {
+    
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/colecciones")
+    public String colecciones(){
+        return "colecciones";
+    }
+
+    @GetMapping("/guia_de_tallas")
+    public String guiatallas() {
+        return "guia_tallas";
+    }
+
+    @GetMapping("/nosotros")
+    public String nosotros() {
+        return "nosotros";
+    }
+
+    @GetMapping("/contactanos")
+    public String contacto() {
+        return "contactanos";
+    }
+    
+}
