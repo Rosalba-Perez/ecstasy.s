f4a52cba6988911af49b2f5ce65024b5fe46998a
Rosalba <rous092006@gmail.com>
2024-10-28 20:17:42 -0500

    -

--- ecstasy/ecstasy/src/main/java/com/ecstasy/s/controlador.java
+++ ecstasy/ecstasy/src/main/java/com/ecstasy/s/controlador.java
@@ -1,5 +1,4 @@
 package com.ecstasy.s;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,30 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class controlador {
-
-    @GetMapping("/colecciones")
-    public String colecciones(){
-        return "colecciones";
-    }
-
-    @GetMapping("/contactanos")
-    public String contacto() {
-        return "contacto";
-    }
-
-    @GetMapping("/guia_de_tallas")
-    public String guiatallas() {
-        return "guiatallas";
-    }
-    
     @GetMapping("/")
-    public String index() {
+    public String index(){
         return "index";
     }
-
-    @GetMapping("/nosotros")
-    public String nosotros() {
-        return "nosotros";
-    }
-
 }
